a77ae1b3027b1662ebef38387ce98c97e2e0945d
nicholoading <nicholas.ting.coding@gmail.com>
2025-02-10 10:15:32 +0800

    updated toaster

--- components/enhancement-submission.tsx
+++ components/enhancement-submission.tsx
@@ -13,7 +13,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { useToast } from "@/components/ui/use-toast";
+import { useToast } from "@/hooks/use-toast";
 import Image from "next/image";
 
 export function EnhancementSubmission() {
